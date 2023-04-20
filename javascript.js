c0fe10cf5a9c719e7b9ade210fb7246ff0c6c9eb
CoderPep13 <97329821+CoderPep13@users.noreply.github.com>
2023-04-20 10:48:26 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,52 @@
+function getPlayerChoice() {
+    let choice = prompt("Rock, Paper, Scissors?","Choose only one!").toLocaleLowerCase();
+    return choice;
+}
+
+function getComputerChoice() {
+    let choice = Math.random();
+    if(choice <= 0.33) {
+        return "rock"
+    } else if(choice >= 0.66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return "It's a draw!"
+    } else if(playerSelection == "rock" && computerSelection == "scissors" 
+            || playerSelection == "scissors" && computerSelection == "paper" 
+            || playerSelection == "paper" && computerSelection == "rock") {
+                playerScore++;
+                return "Player wins!"
+            } else {
+                computerScore++;
+                return "Computer wins!"
+            }
+}
+
+function game() {
+    console.log("Welcome to the game of your life!")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore > computerScore) {
+        console.log("Player won the series!")
+    } else if (playerScore < computerScore) {
+        console.log("Computer won the series!")
+    } else {
+        console.log("No one won!")
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
+game();
